6de632d85196a173edc637bc22eba8215f67b4a5
josy.banegas@loft.com.br <josy.banegas@loft.com.br>
2022-11-26 13:55:04 +0000

    fix stg_shippers and add dim_transportadoras

--- models/staging/erp_northwind/stg_erp_shippers.sql
+++ models/staging/erp_northwind/stg_erp_shippers.sql
@@ -1,11 +1,11 @@
 with
-    source_transportadoras as (
+    source_shippers as (
         select
-            cast(shipper_id	as int) as id_transportadora
-            , cast(company_name as string) as nome_do_transportadora
-            , cast(phone as string) as telefone
+            cast(shipper_id as int) as id_transportadora
+            , cast(company_name as string) as nome_transportadora
+            , cast(phone as string) as telefone_transportadora
         from {{ source('erp', 'shippers') }}
     )
 
 select *
-from source_transportadoras
+from source_shippers
